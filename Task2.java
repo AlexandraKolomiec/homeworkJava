a39314caa62aaacbf87875b0b4fee8ee7771f3dd
Cheshire Cat <Sasha-Km@yandex.ru>
2022-06-21 20:38:05 +0300

    correct Task2

--- Task2.java
+++ Task2.java
@@ -4,18 +4,19 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner iScanner=new Scanner(System.in);
         System.out.println("Введите число: ");
-        double x= iScanner.nextInt();
+        int x= iScanner.nextInt();
         iScanner.close();
-
-        double sum=0;
         
-        while (x>0){
-            sum=sum+x%10;
-            x/=10;
+        int bufNum = x;
+        int sum=0;
+        
+        while (bufNum >0){
+            sum=sum+bufNum %10;
+            bufNum /=10;
             
         }
         System.out.println(sum);
-//эта часть у меня не получилась, не могу разобраться в чем ошибка. в любом случае выдает что число делится на сумму
+
         if (x%sum == 0){
             System.out.println(x%sum);
             System.out.println("число делится на сумму его цифр");
